e59ca12597988e6c34db4d8dcfa19be0ca6aa0db
harsh-909 <hchauhan@909te.ch>
2026-04-22 13:55:25 +0530

    deleted the authentication code from product service as it needs to be added to the user service and updated the db

---
+++ src/main/java/com/ecom/productService/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.ecom.productService.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource, PathPatternRequestMatcher.Builder builder) throws Exception {
+        http
+                .csrf(crsf -> crsf.disable())
+                .cors(cors-> cors.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/products/{id}").authenticated()
+                        .requestMatchers("/products/").hasAuthority("ADMIN")
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+}
